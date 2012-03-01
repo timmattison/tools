00d985654e1e7c9470bc2220f88b37205d9cde42
Tim Mattison <tim@mattison.org>
2012-03-01 08:51:14 -0500

    Updated version number to 0.3   - Extracted is_directory_safe_to_traverse logic in preparation for overall progress code   - Removed old debug code   - Fixed "opendir my" syntax warning.  Added missing call to closedir.   - Implemented overall progress for #6

--- prcp.pl
+++ prcp.pl
@@ -9,7 +9,8 @@
 #   Version 0.2  - 2012-03-01 - Added a carriage return after each file is
 #                                 copied so the users sees all of their statuses
 #                                 not just the last one
-my $version = "0.2";
+#   Version 0.3  - 2012-03-01 - Implemented overall progress option (-o)
+my $version = "0.3";
 my $release_date = "2012-03-01";
 
 # This script was written to provide a copy command with some form of progress indicator.
@@ -82,6 +83,10 @@ if($@) {
 # Assume they don't want these options until they specify them
 my $guarantee = 0;
 my $verify = 0;
+my $overall_progress = 0;
+my $overall_size = 0;
+my $overall_offset = 0;
+my $overall_progress_bar;
 
 # Loop through anything that looks like a switch
 while(@ARGV && ($ARGV[0] =~ m/^-/)) {
@@ -100,6 +105,10 @@ while(@ARGV && ($ARGV[0] =~ m/^-/)) {
       print "Verification enabled [NOT IMPLEMENTED YET]\n";
       $verify = 1;
     }
+    elsif($character eq "o") {
+      # Show overall progress instead of per file progress
+      $overall_progress = 1;
+    }
     else {
       # Unknown, stop immediately
       show_usage();
@@ -111,11 +120,20 @@ while(@ARGV && ($ARGV[0] =~ m/^-/)) {
 }
 
 # Get the input and output files
+my $input = $ARGV[0];
+my $output = $ARGV[1];
+
+# Are we doing an overall progress indicator?
+if($overall_progress == 1) {
+  # Yes, calculate the overall size
+  $overall_size = calculate_overall_size($input);
+  $overall_progress_bar = create_progress_bar($input . " -> " . $output, $overall_size);
+}
 
 # Do we have more than two arguments?
 if(@ARGV == 2) {
   # No, just two arguments.  Do a normal file copy.
-  progress_copy($ARGV[0], $ARGV[1]);
+  progress_copy($input, $output);
 }
 elsif(@ARGV > 2) {
   # Yes, all of the items except the last are files.  The last item must be a directory.
@@ -168,12 +186,19 @@ sub progress_copy {
       # No, the destination does not exist.  Create it.
       mkdir $destination;
 
-      # Let the user know
-      print "Directory $destination created\n";
+      # Are we doing an overall progress indicator?
+      if($overall_progress == 0) {
+        # No, let the user know that we're creating a directory
+        print "Directory $destination created\n";
+      }
+      else {
+        # Yes, show nothing so we don't break the progress indicator
+      }
     }
 
     # Everything looks good.  Copy the source recursively.
-    opendir my $SOURCE_DIRECTORY, $source;
+    my $SOURCE_DIRECTORY;
+    opendir $SOURCE_DIRECTORY, $source;
 
     while(readdir $SOURCE_DIRECTORY) {
       my $directory_entry = $_;
@@ -181,11 +206,13 @@ sub progress_copy {
       my $new_destination = $destination . "/" . $directory_entry;
 
       # Is this entry safe?
-      if(($directory_entry ne ".") && ($directory_entry ne "..")) {
+      if(is_directory_safe_to_traverse($directory_entry)) {
         # Yes, copy it
         progress_copy($new_source, $new_destination);
       }
     }
+
+    closedir $SOURCE_DIRECTORY;
 
     # Return so we don't run this code on a raw directory
     return;
@@ -234,27 +261,27 @@ sub progress_copy {
   # Get the input file's size
   my $filesize = -s $input_file or die "Couldn't get the size of the input file [$input_file]";
 
-  # Check to see if we have the progress bar module
   my $progress_bar;
+  my $offset = 0;
 
-  eval {
-    require Term::ProgressBar;
-    Term::ProgressBar->import();
+  # Do we need a new progress bar?
+  if($overall_progress == 0) {
+    # Yes, we are not doing overall progress we are doing per file progress
+    $progress_bar = create_progress_bar($display_name, $filesize);
+  }
+  else {
+    # No, reuse our overall progress bar
+    $progress_bar = $overall_progress_bar;
 
-    # If we got here the progress bar module is installed.  Instantiate a progress
-    #   bar that measures from 0 to our input file's size so we can use it later.
-    $progress_bar = Term::ProgressBar->new({ name => $display_name,
-                                             count => $filesize,
-                                             ETA => 'linear' });
-  };
+    # Set our offset to the overall offset
+    $offset = $overall_offset;
+  }
 
   open INPUT_FILE, "<$input_file" or die "Couldn't open the input file for reading";
   open OUTPUT_FILE, ">$output_file" or die "Couldn't open the output file for writing";
 
   binmode INPUT_FILE or die "Couldn't go to binary mode on the input file";
   binmode OUTPUT_FILE or die "Couldn't go to binary mode on the output file";
-
-  my $offset = 0;
 
   my $bytes_read = sysread INPUT_FILE, $buffer, $BUFFER_MAX_SIZE;
   check_failure($bytes_read);
@@ -275,8 +302,6 @@ sub progress_copy {
       $progress_bar->update($offset);
     }
 
-    #print "Offset: $offset\n";
-
     $bytes_read = sysread INPUT_FILE, $buffer, $BUFFER_MAX_SIZE;
     check_failure($bytes_read);
   }
@@ -284,9 +309,73 @@ sub progress_copy {
   close INPUT_FILE;
   close OUTPUT_FILE;
 
-  # Move to the next line so that the user can see the status of all of their files,
-  #   not just the last one
-  print "\n";
+  # Are we doing an overall progress bar?
+  if($overall_progress == 0) {
+    # No, move to the next line so that the user can see the status of all of their files
+    #   individually
+    print "\n";
+  }
+  else {
+    # Yes, we are doing an overall progress bar.  Do not insert any carriage returns so
+    #   the overall status stays on the same line.
+
+    # Update the overall offset
+    $overall_offset = $offset;
+  }
+}
+
+sub calculate_overall_size {
+  my $source = $_[0];
+  my $size = 0;
+
+  # Is the source a directory?
+  if(-d $source) {
+    # Everything looks good.  Copy the source recursively.
+    my $SOURCE_DIRECTORY;
+    opendir $SOURCE_DIRECTORY, $source;
+
+    while(readdir $SOURCE_DIRECTORY) {
+      my $directory_entry = $_;
+      my $new_source = $source . "/" . $directory_entry;
+
+      # Is this a file?
+      if(-f $new_source) {
+        # Yes, just add its size to the total
+        $size += -s $new_source;
+      }
+      else {
+        # No, is this a directory?
+        if(-d $new_source) {
+          # Yes, is this directory safe?
+          if(is_directory_safe_to_traverse($directory_entry)) {
+            # Yes, add its size to the total size
+            $size += calculate_overall_size($new_source);
+          }
+        }
+      }
+    }
+
+    closedir $SOURCE_DIRECTORY;
+  }
+
+  # Return the total size
+  return $size;
+}
+
+sub is_directory_safe_to_traverse {
+  my $directory_entry = $_[0];
+
+  # Is this directory safe to traverse?
+  if(($directory_entry ne ".") && ($directory_entry ne "..")) {
+    # Yes, it is not "." (the current directory which would cause an infinite loop) and it
+    #   is not ".." (which would cause another infinite loop by going up the file system
+    #   to the parent directory)
+    return 1;
+  }
+  else {
+    # No, this is not safe
+    return 0;
+  }
 }
 
 sub check_failure {
@@ -307,4 +396,23 @@ sub show_usage {
   print "  NOTE: File verification will increase the amount of time required to copy files!\n";
   print "\n";
   exit;
+}
+
+sub create_progress_bar {
+  my $display_name = $_[0];
+  my $filesize = $_[1];
+  my $progress_bar;
+
+  eval {
+    require Term::ProgressBar;
+    Term::ProgressBar->import();
+
+    # If we got here the progress bar module is installed.  Instantiate a progress
+    #   bar that measures from 0 to our input file's size so we can use it later.
+    $progress_bar = Term::ProgressBar->new({ name => $display_name,
+                                             count => $filesize,
+                                             ETA => 'linear' });
+  };
+
+  return $progress_bar;
 }
