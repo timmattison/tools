5887a7aa26cd1708d477365ec50f9cec95eca920
Tim Mattison <tim@mattison.org>
2012-03-16 15:51:16 -0400

    Added load average logging

--- logload.pl
+++ logload.pl
@@ -7,7 +7,8 @@ use Data::Dumper;
 
 # Release history:
 #   Version 0.1  - 2012-03-16 - First release
-my $version = "0.1";
+#   Version 0.2  - 2012-03-16 - Added secondary load average log file
+my $version = "0.2";
 my $release_date = "2012-03-16";
 
 # This script was written to log system performance characteristics in the simplest way
@@ -18,8 +19,9 @@ my $release_date = "2012-03-16";
 #   intended to give you the raw data so you can process it on your own.  In the future
 #   I may add on some features to make this easier.
 
-# The file to which we're going to write our output
-my $OUTPUT_FILE = "cpu-load.log";
+# The files to which we're going to write our output
+my $CPU_OUTPUT_FILE = "cpu-load.log";
+my $LOAD_AVERAGE_OUTPUT_FILE = "load-average.log";
 
 # How long we should wait and gather statistics
 my $SLEEP_TIME = 5;
@@ -48,15 +50,26 @@ sleep $SLEEP_TIME;
 # Get the stats
 my $stats = $stat_grabber->get;
 
-# Does the output file exist?
-if(!-e $OUTPUT_FILE) {
+# Does the CPU output file exist?
+if(!-e $CPU_OUTPUT_FILE) {
   # No, create it and print the header info
-  open OUTPUT_FILE, ">$OUTPUT_FILE";
-  print OUTPUT_FILE "CPU Number,Reading Time,System,SoftIRQ,Idle,Nice,IRQ,Steal,User,IOWait,Total\n";
+  open CPU_OUTPUT_FILE, ">$CPU_OUTPUT_FILE";
+  print CPU_OUTPUT_FILE "CPU Number,Reading Time,System,SoftIRQ,Idle,Nice,IRQ,Steal,User,IOWait,Total\n";
 }
 else {
-  # Open the output file for appending
-  open OUTPUT_FILE, ">>$OUTPUT_FILE";
+  # Open the CPU output file for appending
+  open CPU_OUTPUT_FILE, ">>$CPU_OUTPUT_FILE";
+}
+
+# Does the load average output file exist?
+if(!-e $LOAD_AVERAGE_OUTPUT_FILE) {
+  # No, create it and print the header info
+  open LOAD_AVERAGE_OUTPUT_FILE, ">$LOAD_AVERAGE_OUTPUT_FILE";
+  print LOAD_AVERAGE_OUTPUT_FILE "Reading Time,5 minute,10 minute,15 minute,Number of processes,Most recent PID\n";
+}
+else {
+  # Open the load average output file for appending
+  open LOAD_AVERAGE_OUTPUT_FILE, ">>$LOAD_AVERAGE_OUTPUT_FILE";
 }
 
 # Loop through the CPUs
@@ -72,33 +85,45 @@ while(defined($current_record)) {
   # Get the current record
 
   # Print out the fields in the order we expect
-  print OUTPUT_FILE $cpu_number;
-  print OUTPUT_FILE ",";
-  print OUTPUT_FILE $epoch;
-  print OUTPUT_FILE ",";
-  print OUTPUT_FILE $current_record->{system};
-  print OUTPUT_FILE ",";
-  print OUTPUT_FILE $current_record->{softirq};
-  print OUTPUT_FILE ",";
-  print OUTPUT_FILE $current_record->{idle};
-  print OUTPUT_FILE ",";
-  print OUTPUT_FILE $current_record->{nice};
-  print OUTPUT_FILE ",";
-  print OUTPUT_FILE $current_record->{irq};
-  print OUTPUT_FILE ",";
-  print OUTPUT_FILE $current_record->{steal};
-  print OUTPUT_FILE ",";
-  print OUTPUT_FILE $current_record->{user};
-  print OUTPUT_FILE ",";
-  print OUTPUT_FILE $current_record->{iowait};
-  print OUTPUT_FILE ",";
-  print OUTPUT_FILE $current_record->{total};
-  print OUTPUT_FILE "\n";
+  print CPU_OUTPUT_FILE $cpu_number;
+  print CPU_OUTPUT_FILE ",";
+  print CPU_OUTPUT_FILE $epoch;
+  print CPU_OUTPUT_FILE ",";
+  print CPU_OUTPUT_FILE $current_record->{system};
+  print CPU_OUTPUT_FILE ",";
+  print CPU_OUTPUT_FILE $current_record->{softirq};
+  print CPU_OUTPUT_FILE ",";
+  print CPU_OUTPUT_FILE $current_record->{idle};
+  print CPU_OUTPUT_FILE ",";
+  print CPU_OUTPUT_FILE $current_record->{nice};
+  print CPU_OUTPUT_FILE ",";
+  print CPU_OUTPUT_FILE $current_record->{irq};
+  print CPU_OUTPUT_FILE ",";
+  print CPU_OUTPUT_FILE $current_record->{steal};
+  print CPU_OUTPUT_FILE ",";
+  print CPU_OUTPUT_FILE $current_record->{user};
+  print CPU_OUTPUT_FILE ",";
+  print CPU_OUTPUT_FILE $current_record->{iowait};
+  print CPU_OUTPUT_FILE ",";
+  print CPU_OUTPUT_FILE $current_record->{total};
+  print CPU_OUTPUT_FILE "\n";
 
   # Move onto the next CPU
   $cpu_number++;
   $current_record = $stats->{"cpu" . $cpu_number};
 }
 
-# Close the file
-close OUTPUT_FILE;
+# Close the CPU output file
+close CPU_OUTPUT_FILE;
+
+# Get the load average
+my $load_average = `cat /proc/loadavg`;
+
+# Replace the spaces with commas
+$load_average =~ s/ /,/g;
+
+# Put it in our output file
+print LOAD_AVERAGE_OUTPUT_FILE $load_average;
+
+# Close the load average output file
+close LOAD_AVERAGE_OUTPUT_FILE;
